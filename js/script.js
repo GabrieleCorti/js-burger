bbf46e62cceb92475b9b1bab79539e070cecc8c9
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-05 17:55:55 +0100

    fine js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
 var calcolaPrezzo = document.getElementById('calcola-prezzo');
-//var burgerName = document.getElementById("burgername").value;
+var burgerName = document.getElementById("burgername");
 var checkBox = document.getElementsByClassName("box");
-
+var sum = 50;
+var prezzo = document.getElementById("prezzo");
+var couponList = ["124", "4321", "1234qbc", "qbc1234"]
+var coupon = document.getElementById("coupon");
 //ascolto click
 
 calcolaPrezzo.addEventListener("click",
   function() {
 
-    if (document.getElementById("burgername").value != ""){
+    if ( burgerName.value != ""){
       //scorre fra la lista di cekbox e guarda se sono cekati
-      var sum = 50;
+
       for (var i = 0; i < checkBox.length; i++) {
 
         if (checkBox[i].checked == true) {
-
+          //sommo
           sum += parseInt(checkBox[i].value);
 
         }
 
       }
-      console.log(sum);
 
     } else {
 
       alert("inserisci nome hamburger");
+
+    }
+    //applico sconto se c'è coupon
+    if (couponList.includes(coupon.value)) {
+
+      prezzo.innerHTML = Math.trunc(sum * 0.8) + "$";
+
+    } else {
+
+      prezzo.innerHTML = sum + "$";
 
     }
 
